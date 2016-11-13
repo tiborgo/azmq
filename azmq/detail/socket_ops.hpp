81e08eb196f4485c1d15c1c8637a380f3428ca2e
Tibor Goldschwendt <tibor.goldschwendt@googlemail.com>
2016-11-13 02:30:29 +0100

    Removed boost.regex.

--- azmq/detail/socket_ops.hpp
+++ azmq/detail/socket_ops.hpp
@@ -16,7 +16,7 @@
 #include <boost/assert.hpp>
 #include <boost/format.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/regex.hpp>
+#include <regex>
 #include <boost/utility/string_ref.hpp>
 #include <asio/io_service.hpp>
 #include <asio/socket_base.hpp>
@@ -123,14 +123,14 @@ namespace detail {
                                               endpoint_type & ep,
                                               asio::error_code & ec) {
             BOOST_ASSERT_MSG(socket, "invalid socket");
-            const boost::regex simple_tcp("^tcp://.*:(\\d+)$");
-            const boost::regex dynamic_tcp("^(tcp://.*):([*!])(\\[(\\d+)?-(\\d+)?\\])?$");
-            boost::smatch mres;
+            const std::regex simple_tcp("^tcp://.*:(\\d+)$");
+            const std::regex dynamic_tcp("^(tcp://.*):([*!])(\\[(\\d+)?-(\\d+)?\\])?$");
+            std::smatch mres;
             int rc = -1;
-            if (boost::regex_match(ep, mres, simple_tcp)) {
+            if (std::regex_match(ep, mres, simple_tcp)) {
                 if (zmq_bind(socket.get(), ep.c_str()) == 0)
                     rc = boost::lexical_cast<uint16_t>(mres.str(1));
-            } else if (boost::regex_match(ep, mres, dynamic_tcp)) {
+            } else if (std::regex_match(ep, mres, dynamic_tcp)) {
                 auto const& hostname = mres.str(1);
                 auto const& opcode = mres.str(2);
                 auto const& first_str = mres.str(4);
